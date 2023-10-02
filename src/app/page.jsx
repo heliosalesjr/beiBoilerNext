b4d372de4edbc1015a008a01ca74134dec19e4a2
heliosalesjr <heliosalesjr@gmail.com>
2023-10-02 14:13:18 -0300

    updated global styles and tailwind config

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,11 @@ import Hero from './components/Hero';
 import FAQ from './components/FAQ';
 import CollapseHere from './components/CollapseHere'
 import Cardy from './components/Cardy';
+import Footer from './components/Footer';
 //import Link from 'next/link';
 import NavbarDark from './components/NavbarDark';
 import CarouselHere from './components/CarouselHere';
+
 
 
 export default function Home() {
@@ -13,15 +15,15 @@ export default function Home() {
   
   return (
     <>
-      <NavbarDark />
-      <Hero />
-        <FAQ />
-        <CollapseHere />
-        <CarouselHere />
-        <div className="flex flex-col items-center px-20 md:flex-row md:justify-center md:space-x-5 space-y-5">
-            <Cardy /><Cardy /><Cardy />
-        </div>
       
+      <Hero />
+      <FAQ />
+      <CollapseHere />
+      <CarouselHere />
+      <div className="flex flex-col items-center px-20 md:flex-row md:justify-center md:space-x-5 space-y-5">
+          <Cardy /><Cardy /><Cardy />
+      </div>
+      <Footer />
     
     </>
   )
